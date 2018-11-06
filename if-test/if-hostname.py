8eb24194300227d42a831656b525ddfc7688f827
Roman Navarro <romanoff37@hotmail.com>
2018-11-06 16:23:12 -0500

    your commit message

---
+++ if-test/if-hostname.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+print ('please provide the name of the host')
+hostname = input().upper()
+if hostname == 'MTG':
+  print('hostname matches expected config')
+print ('Exiting the script')
